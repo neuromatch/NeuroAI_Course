c9cb8afa334dcfe41ed1b1784316bf7d65ed7d01
glibesyck <71762371+glibesyck@users.noreply.github.com>
2024-04-09 09:56:43 +0300

    naming update

--- tutorials/W2D4_MacroLearning/solutions/W2D4_Tutorial1_Solution_c9163be1.py
+++
@@ -1,6 +0,0 @@
-
-"""
-Discussion: Why do you think R-squared value is still higher for this particular example of concept shift in comparison to the covariate one?
-
-In this example, concept shift preserves the annual and weekly trends (we can see that predicitions oscillate the same way as the shifted function), thus R-squared value is relatively high.
-""";
