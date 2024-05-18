6e1d71d1ff882703f58b98409c1be23567815a35
glibesyck <gsolodzhuk@gmail.com>
2024-05-18 18:26:07 +0300

    final text and slides; only exepcted change - videos

--- tutorials/W2D4_Macrolearning/solutions/W2D4_Tutorial1_Solution_ee9c804d.py
+++
@@ -1,7 +0,0 @@
-
-"""
-Discussion: How would you qualitatively evaluate the model performance on autumn data? Does it capture the annual trend? Does it capture weekly trend?
-
-Model predictions are completely invariant to the weekly seasonality of the data, though it somewhat captures the increasing trend.
-Thus, definitely, it can't be used for making quality predictions on the daily basis.
-""";
