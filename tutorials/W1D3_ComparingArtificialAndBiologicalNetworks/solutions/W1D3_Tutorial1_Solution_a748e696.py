3dac0ae6091f642874128cce70d7cdba0ab37a9c
GitHub Action <action@github.com>
2024-05-24 06:30:32 +0000

    Process tutorial notebooks

---
+++ tutorials/W1D3_ComparingArtificialAndBiologicalNetworks/solutions/W1D3_Tutorial1_Solution_a748e696.py
@@ -0,0 +1,7 @@
+
+"""
+Question: For adversarial images, how do the RDMs change when comparing representations from the standard model to the adversarially trained model?
+
+Note for adversarial images, the representation similarity of the standard model within each category (the block effect) is notably disrupted in the deeper layers.
+Conversely, for the adversarially trained model, despite the introduction of adversarial examples, the representation similarity matrix for preserves its block-like structure across categories.
+""";
