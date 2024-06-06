816e4d844154a50d0e03c177135090b3bcaa9465
GitHub Action <action@github.com>
2024-06-06 14:52:39 +0000

    Process tutorial notebooks

--- tutorials/W1D5_Microcircuits/solutions/W1D5_Tutorial1_Solution_6c86015c.py
+++
@@ -1,5 +0,0 @@
-# define a list or numpy array of optional cardinallities from 1 to 51 in intervals of 5.
-cardinalities = np.arange(1,101,5)
-
-# for each of the optional cardinalities, run OMP using the pixel's signal from before. Create a list called "regs" that include all OMP's fitted objects
-regs = [OrthogonalMatchingPursuit(fit_intercept = True, n_nonzero_coefs = card).fit(np.vstack(set_sigs).T, sig) for card in cardinalities]
--- tutorials/W1D5_Microcircuits/solutions/W1D5_Tutorial1_Solution_97753bc0.py
+++
@@ -1,3 +0,0 @@
-low_perc = np.percentile(frame, 80)
-frame_HT = hard_thres(frame, low_perc)
-plot_images([frame, frame_HT])
