d288d2737058d1db2a27b9c964c743790741d4e6
GitHub Action <action@github.com>
2024-06-10 13:33:06 +0000

    Process tutorial notebooks

--- tutorials/W1D3_ComparingArtificialAndBiologicalNetworks/solutions/W1D3_Tutorial4_Solution_1ac2083f.py
+++ tutorials/W1D3_ComparingArtificialAndBiologicalNetworks/solutions/W1D3_Tutorial4_Solution_1ac2083f.py
@@ -1,6 +1,6 @@
 
 stim_idx = [0,1] # change stimulus index to visualize another pair of stimuli
-d_dims_list = np.power(2, np.arange(1,10))
+m_dims_list = np.power(2, np.arange(1,10))
 true_dist, projected_dist = {}, {}
 for i, n_neurons in enumerate(n_neurons_list):
     data = clean_dataset[n_neurons].sel({"stim": stim_idx})
@@ -8,13 +8,13 @@ for i, n_neurons in enumerate(n_neurons_list):
     true_dist[n_neurons] = calc_rdm(data, method='euclidean', noise=None, normalize_by_channels=False).dissimilarities.item()
 
     projected_dist[n_neurons]=[]
-    for d_dims in d_dims_list:
-        A = np.random.normal(loc=0, scale=1, size=(n_neurons, d_dims))
-        A *= np.sqrt(1/d_dims)
+    for m_dims in m_dims_list:
+        A = np.random.normal(loc=0, scale=1, size=(n_neurons, m_dims))
+        A *= np.sqrt(1/m_dims)
         transformed_data = (data.values @ A)
-        transformed_data = np2xr(transformed_data, coords={'stim': data.stim.values, 'neuron': np.arange(d_dims)})
+        transformed_data = np2xr(transformed_data, coords={'stim': data.stim.values, 'neuron': np.arange(m_dims)})
         rdm = calc_rdm(transformed_data, method='euclidean', noise=None, normalize_by_channels=False)
         projected_dist[n_neurons].append(rdm.dissimilarities.item())
     projected_dist[n_neurons] = np.array(projected_dist[n_neurons])
 
-plot_distance_after_projection(true_dist, projected_dist, n_neurons_list, d_dims_list)
+plot_distance_after_projection(true_dist, projected_dist, n_neurons_list, m_dims_list)
