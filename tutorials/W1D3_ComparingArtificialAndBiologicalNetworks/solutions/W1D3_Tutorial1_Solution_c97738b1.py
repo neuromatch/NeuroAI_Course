c8335316740c3d0d8c14dc8aa11bd10866264d3b
GitHub Action <action@github.com>
2024-06-14 08:01:23 +0000

    Process tutorial notebooks

--- tutorials/W1D3_ComparingArtificialAndBiologicalNetworks/solutions/W1D3_Tutorial1_Solution_c97738b1.py
+++
@@ -1,6 +0,0 @@
-
-"""
-Question: For adversarial images, how do the RDMs change when comparing representations from the standard model to the adversarially trained model?
-
-For clean images representing the same digit, their representations in the deeper layers of the network are remarkably similar and align well with the inherent category structure, manifesting as a distinct block effect. In contrast, this block effect is less pronounced in the earlier layers of the network. The initial layers focus more on capturing general and granular visual features. This progression from generic to more refined feature extraction across layers underscores the hierarchical nature of learning in deep neural networks, where complex representations are built upon the simpler ones extracted at earlier stages.
-""";
