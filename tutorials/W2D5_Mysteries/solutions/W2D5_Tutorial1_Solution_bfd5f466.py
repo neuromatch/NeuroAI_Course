cb52b26f38c373771bbc1f233b08b044c05e6f52
GitHub Action <action@github.com>
2024-08-07 21:14:29 +0000

    Process tutorial notebooks

---
+++ tutorials/W2D5_Mysteries/solutions/W2D5_Tutorial1_Solution_bfd5f466.py
@@ -0,0 +1,9 @@
+"""
+They both agree that ignition happens, i.e that it's a key empirical phenomenon that needs explaining.
+The difference is whether it is assumed to be constitutive of consciousness, or a by-product of computations
+related to consciousness. GWS says that it's constitutive - global broadcast underpins conscious access,
+and ignition is the neural signature of broadcast. HOSS says that ignition is a by-product of computations
+related to consciousness. Under HOSS, consciousness is tied to the higher-order evaluation of the
+reliability/strength of first-order state(s) - and when a first-order state is reliable / precise,
+it will also be associated with widespread updates to the internal model which manifest as ignition.
+"""
