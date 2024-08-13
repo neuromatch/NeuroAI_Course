a5902a8e2f30a9b8b4d65c8f12bfb400c10de033
GitHub Action <action@github.com>
2024-08-13 10:38:23 +0000

    Process tutorial notebooks

--- tutorials/W2D4_Macrolearning/solutions/W2D4_Tutorial2_Solution_ca46562e.py
+++ tutorials/W2D4_Macrolearning/solutions/W2D4_Tutorial2_Solution_ca46562e.py
@@ -12,7 +12,3 @@ for _ in tqdm(range(num_epochs - 1), desc="Training Progress"):
     # Calculate r-squared values on test sets
     summer_r_squared.append(base_model.score(summer_days_test_norm, summer_prices_test))
     autumn_r_squared.append(base_model.score(autumn_days_test_norm, autumn_prices_test))
-
-# Final model and plot
-model = base_model
-plot_performance(num_epochs, summer_r_squared, autumn_r_squared)
