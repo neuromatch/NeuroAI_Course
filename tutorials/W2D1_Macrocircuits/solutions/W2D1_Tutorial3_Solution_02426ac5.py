dca2fb200e57e02ff4d3daf8a21e7f18ef888da0
GitHub Action <action@github.com>
2024-08-14 22:37:35 +0000

    Process tutorial notebooks

---
+++ tutorials/W2D1_Macrocircuits/solutions/W2D1_Tutorial3_Solution_02426ac5.py
@@ -0,0 +1,6 @@
+
+"""
+Discussion: Is there any difference between the trajectories for the modular and holistic agents? If so, what does it imply?
+
+The holistic agent's trajectory has a higher curvature and length than that of the modular agent, suggesting that the modular agent's trajectory is more optimal. This is because, based on the RL objective with a discount factor smaller than 1, the trajectory should be as efficient (involving fewer steps) as possible.
+"""
