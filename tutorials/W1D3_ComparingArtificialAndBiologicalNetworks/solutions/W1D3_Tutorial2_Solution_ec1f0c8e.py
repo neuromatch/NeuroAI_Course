f5b9eda78379e07ac612aa9f83a685fc3b9414e6
GitHub Action <action@github.com>
2024-08-21 15:36:59 +0000

    Process tutorial notebooks

--- tutorials/W1D3_ComparingArtificialAndBiologicalNetworks/solutions/W1D3_Tutorial2_Solution_ec1f0c8e.py
+++
@@ -1,6 +0,0 @@
-
-return_layers = ['input', 'conv1', 'conv2', 'fc1', 'fc2']
-imgs, labels = sample_images(test_loader, n=50) # grab 500 samples from the test set
-model_features = extract_features(model_robust, imgs.to(device), return_layers)
-
-plot_dim_reduction(model_features, labels, transformer_funcs =['MDS'])
